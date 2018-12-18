d90ac20d56412be3210bceb5d4f1b5174e8c5be1
jethet <henriette.hettinga@gmail.com>
2018-12-18 16:00:53 +0100

    Test choice loop

--- TEST.py
+++ TEST.py
@@ -1,7 +1,3 @@
-"""
-This is the function that asks the player to choose where to put a mark.
-The function first checks if the choice is valid.
-"""
 def choice():
     from beautifultable import BeautifulTable
     board = BeautifulTable()
@@ -11,9 +7,15 @@ def choice():
     print(board, '\n')
     square = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     choice = int(input("Where do you place your mark? "))
-    if choice in square:
+    while choice in square:
         if choice == 1:
             board[0][0] = 'X'
+            print(board)
         elif choice == 2:
             board[0][1] = 'X'
-    print(board)
+            print(board)
+        break
+    return board
+
+
+choice()
