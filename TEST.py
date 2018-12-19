2a995b6ec6276730dc1e51173cbf037d17c00127
jethet <henriette.hettinga@gmail.com>
2018-12-19 16:43:51 +0100

    Raises exception

--- TEST.py
+++ TEST.py
@@ -15,28 +15,27 @@ def choice():
                 square = [1, 2, 3, 4, 5, 6, 7, 8, 9]
                 player = int(input("Are you player 1 or player 2? "))
                 choice = int(input("Where do you place your mark? "))
-                if choice in square:
-                    if player == 1 or player == 2:
-                        with open('board', mode = 'rb') as my_file:
-                            board = pickle.load(my_file)
-                            if choice == 1 and player == 1:
-                                board[0][0] = 'X'
+                if choice in square and player == 1 or player == 2:
+                    with open('board', mode = 'rb') as my_file:
+                        board = pickle.load(my_file)
+                        if choice == 1 and player == 1:
+                            board[0][0] = 'X'
+                            square.remove(1)
+                            print(board)
+                            with open('board', mode = 'wb') as my_file:
+                                pickle.dump(board, my_file)
+
+                        elif choice == 1 and player == 2:
+                            with open('board', mode = 'rb') as my_file:
+                                board = pickle.load(my_file)
+                                board[0][0] = 'O'
                                 square.remove(1)
                                 print(board)
                                 with open('board', mode = 'wb') as my_file:
-                                    pickle.dump(board, my_file)
-
-                            elif choice == 1 and player == 2:
-                                with open('board', mode = 'rb') as my_file:
-                                    board = pickle.load(my_file)
-                                    board[0][0] = 'O'
-                                    square.remove(1)
-                                    print(board)
-                                    with open('board', mode = 'wb') as my_file:
                                         pickle.dump(board, my_file)
 
-                    else:
-                        print("Choose between 1 and 2. ")
+                else:
+                    print("Choose between 1 and 2. ")
             game = False
     except:
         print("Please enter a number. ")
