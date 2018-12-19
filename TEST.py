2ea9f2b190c95bdeb81071011f59b900043bc432
jethet <henriette.hettinga@gmail.com>
2018-12-19 17:15:08 +0100

    Board changes saved through pickle

--- TEST.py
+++ TEST.py
@@ -7,23 +7,27 @@ def choice():
         board.append_row(['1', '2', '3'])
         board.append_row(['4', '5', '6'])
         board.append_row(['7', '8', '9'])
-        with open('board', mode = 'wb') as my_file:
-            pickle.dump(board, my_file)
-            print(board)
-            game = False
-            while game == False:
-                square = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-                player = int(input("Are you player 1 or player 2? "))
-                choice = int(input("Where do you place your mark? "))
-                if choice in square and player == 1 or player == 2:
-                    if choice == 1 and player == 1:
+        print(board)
+        game = False
+        while game == False:
+            square = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+            player = int(input("Are you player 1 or player 2? "))
+            choice = int(input("Where do you place your mark? "))
+            if choice in square and player == 1 or player == 2:
+                if choice == 1 and player == 1:
+                    with open('board', mode = 'wb') as my_file:
+                        pickle.dump(board, my_file)
+                        print(board)
                         board[0][0] = 'X'
                         square.remove(1)
                         print(board)
                         with open('board', mode = 'wb') as my_file:
                             pickle.dump(board, my_file)
 
-                    elif choice == 1 and player == 2:
+                elif choice == 1 and player == 2:
+                    with open('board', mode = 'wb') as my_file:
+                        pickle.dump(board, my_file)
+                        print(board)
                         board[0][0] = 'O'
                         square.remove(1)
                         print(board)
