0f584f08e05a11afdc903fe9124cb26971b6c43e
jethet <henriette.hettinga@gmail.com>
2018-12-19 15:21:26 +0100

    Board choice and player alternate

--- TEST.py
+++ TEST.py
@@ -5,17 +5,29 @@ def choice():
     board.append_row(['4', '5', '6'])
     board.append_row(['7', '8', '9'])
     print(board, '\n')
+
     square = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    player = int(input("Are you player 1 or player 2? "))
     choice = int(input("Where do you place your mark? "))
-    while choice in square:
-        if choice == 1:
+    if choice in square:
+        if choice == 1 and player == 1:
             board[0][0] = 'X'
+            square.remove(1)
+            print(board)
+        if choice == 1 and player == 2:
+            board[0][0] = 'O'
+            square.remove(1)
             print(board)
         elif choice == 2:
             board[0][1] = 'X'
             print(board)
-        break
-    return board
-
+        elif choice == 3:
+            board[0][2] = 'X'
+            print(board)
+        elif choice == 4:
+            board[1][0] = 'X'
+            print(board)
+        else:
+            print("Please choose a number between 1 and 9. ")
 
 choice()
