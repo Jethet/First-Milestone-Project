ebdf5a5f9578872922214fc51661b2ca6c02c695
jethet <henriette.hettinga@gmail.com>
2018-12-19 15:37:57 +0100

    Use try/except for string error

--- TEST.py
+++ TEST.py
@@ -6,19 +6,26 @@ def choice():
     board.append_row(['7', '8', '9'])
     print(board, '\n')
 
-    square = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    player = int(input("Are you player 1 or player 2? "))
-    choice = int(input("Where do you place your mark? "))
-    if choice in square:
-        if choice == 1 and player == 1:
-            board[0][0] = 'X'
-            square.remove(1)
-            print(board)
-        if choice == 1 and player == 2:
-            board[0][0] = 'O'
-            square.remove(1)
-            print(board)
-        elif choice == 2:
+    try:
+        game = False
+        while game == False:
+            square = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+            player = int(input("Are you player 1 or player 2? "))
+            choice = int(input("Where do you place your mark? "))
+            if choice in square:
+                if choice == 1 and player == 1:
+                    board[0][0] = 'X'
+                    square.remove(1)
+                    print(board)
+                elif choice == 1 and player == 2:
+                    board[0][0] = 'O'
+                    square.remove(1)
+                    print(board)                    
+        game = False
+    except:
+        print("Please choose a number between 1 and 9. ")
+"""
+            elif choice == 2:
             board[0][1] = 'X'
             print(board)
         elif choice == 3:
@@ -27,7 +34,7 @@ def choice():
         elif choice == 4:
             board[1][0] = 'X'
             print(board)
-        else:
-            print("Please choose a number between 1 and 9. ")
+"""
+
 
 choice()
