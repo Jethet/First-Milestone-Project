1f99c13232430ea50ab77ec864fe2c39af063867
jethet <henriette.hettinga@gmail.com>
2018-12-20 12:41:05 +0100

    Edit pickle/unpickle

--- TEST.py
+++ TEST.py
@@ -7,28 +7,32 @@ def choice():
     board.append_row(['4', '5', '6'])
     board.append_row(['7', '8', '9'])
     print(board)
-    while square == ['1', '2', '3', '4', '5', '6', '7', '8', '9']:
+    with open('board', mode = 'wb') as my_file:
+        pickle.dump(board, my_file)
         player = int(input("Are you player 1 or player 2? "))
-        choice = input("Where do you place your mark? ")
-        if choice in square and player == 1 or player == 2:
-            if choice == 1 and player == 1:
-                with open('board', mode = 'wb') as my_file:
-                    pickle.dump(board, my_file)
-                    board[0][0] = 'X'
-                    square.remove('1')
-                    print(board)
-                    with open('board', mode = 'wb') as my_file:
-                        pickle.dump(board, my_file)
+        while player == 1 or player == 2:
+            square = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+            choice = input("Where do you place your mark? ")
+            if choice in square:
+                with open('board', mode = 'rb') as my_file:
+                    board = pickle.load(my_file)
+                    if choice == 1 and player == 1:
+                        board[0][0] = 'X'
+                        square.remove('1')
+                        print(board)
+                        with open('board', mode = 'wb') as my_file:
+                            pickle.dump(board, my_file)
 
-            elif choice == 1 and player == 2:
-                with open('board', mode = 'wb') as my_file:
-                    pickle.dump(board, my_file)
-                    board[0][0] = 'O'
-                    square.remove('1')
-                    print(board)
-                    with open('board', mode = 'wb') as my_file:
-                        pickle.dump(board, my_file)
+                    elif choice == 1 and player == 2:
+                        board[0][0] = 'O'
+                        square.remove('1')
+                        print(board)
+                        with open('board', mode = 'wb') as my_file:
+                            pickle.dump(board, my_file)
+            else:
+                print("Choose between 1 and 2. ")
         else:
-            print("Choose between 1 and 2. ")
+            print("This is not a valid choice.")
+
 
 choice()
