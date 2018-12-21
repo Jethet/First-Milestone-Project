9e7a2bd09389d6a5290e568b94f42914f07ebfe4
jethet <henriette.hettinga@gmail.com>
2018-12-21 16:22:24 +0100

    Use True for loop

--- TTTgame.py
+++ TTTgame.py
@@ -11,48 +11,49 @@ def choice():
     board.append_row(['4', '5', '6'])
     board.append_row(['7', '8', '9'])
     print(board)
-    board = False
     with open('board', mode = 'wb') as my_file:  #this is meant to save the board
         pickle.dump(board, my_file)
-    while board == False:
-        player = int(input("Are you player 1 or player 2? "))
-        square = ['1', '2', '3', '4', '5', '6', '7', '8', '9'] #3 x 3 squares on board
-        choice = input("Where do you place your mark? ")
-        if choice == '1' and player == 1:
-            board[0][0] = 'X'       #player 1 uses 'X' as mark
-            square.remove('1') #the chosen square is no longer an option
-            print(board)
-            with open('board', mode = 'wb') as my_file:
-                    pickle.dump(board, my_file)           #the new board with 'X' is printed
-        elif choice == '2' and player == 1:
-            board[0][1] = 'X'
-            square.remove('2')
-            print(board)
-            with open('board', mode = 'wb') as my_file:
-                    pickle.dump(board, my_file)
-        elif choice == '3' and player == 1:
-            board[0][2] = 'X'
-            square.remove('3')
-            print(board)
-            with open('board', mode = 'wb') as my_file:
-                    pickle.dump(board, my_file)
+    try:
+        while True:
+            player = int(input("Are you player 1 or player 2? "))
+            square = ['1', '2', '3', '4', '5', '6', '7', '8', '9'] #3 x 3 squares on board
+            choice = input("Where do you place your mark? ")
+            if choice == '1' and player == 1:
+                board[0][0] = 'X'       #player 1 uses 'X' as mark
+                square.remove('1') #the chosen square is no longer an option
+                print(board)
+                with open('board', mode = 'wb') as my_file:
+                        pickle.dump(board, my_file)           #the new board with 'X' is printed
+            elif choice == '2' and player == 1:
+                board[0][1] = 'X'
+                square.remove('2')
+                print(board)
+                with open('board', mode = 'wb') as my_file:
+                        pickle.dump(board, my_file)
+            elif choice == '3' and player == 1:
+                board[0][2] = 'X'
+                square.remove('3')
+                print(board)
+                with open('board', mode = 'wb') as my_file:
+                        pickle.dump(board, my_file)
 
-        elif choice == '1' and player == 2:
-            board[0][0] = 'O'
-            square.remove('1')
-            print(board)
-            with open('board', mode = 'wb') as my_file:
-                    pickle.dump(board, my_file)
-        elif choice == '2' and player == 2:
-            board[0][1] = 'O'
-            square.remove('2')
-            print(board)
-            with open('board', mode = 'wb') as my_file:
-                    pickle.dump(board, my_file)
+            elif choice == '1' and player == 2:
+                board[0][0] = 'O'
+                square.remove('1')
+                print(board)
+                with open('board', mode = 'wb') as my_file:
+                        pickle.dump(board, my_file)
+            elif choice == '2' and player == 2:
+                board[0][1] = 'O'
+                square.remove('2')
+                print(board)
+                with open('board', mode = 'wb') as my_file:
+                        pickle.dump(board, my_file)
 
-        else:
-            print("This is not a valid choice.")
-            return
-    board = False
-    
+            else:
+                print("This is not a valid choice.")
+                return
+    except:
+        print("This is not a valid entry.")
+        
 choice()
