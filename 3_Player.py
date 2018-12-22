07656114685c8548f0d0816c34e08c79f84782a0
jethet <henriette.hettinga@gmail.com>
2018-12-22 14:49:11 +0100

    Function to determine player X and player O

--- 3_Player.py
+++ 3_Player.py
@@ -5,11 +5,16 @@ def player():
     start = input("Would you like to play tic tac toe? Choose 1 for yes and 2 for no. ")
     if start == '2':
         print("Game over.")
+    elif start != '1' and start !='2':
+        print("This is not a valid entry.")
     else:
         player = input("Choose which player you want to be: 1 for 'X' and 2 for '0'. ")
         if player == '1':
             print("Player 1 will start: you can choose first.")
+        elif player == '2':
+            print("You are player 2. Player 1 gets the first turn.")
         else:
-            print("Player 1 always gets the first turn.")
+            print("This is not a valid entry.")
+
 
 player()
