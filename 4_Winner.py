da2fa207f1dfde8a50ec2bcc9a204cc25cc57e0f
jethet <henriette.hettinga@gmail.com>
2018-12-22 15:42:12 +0100

    Test code for winner

--- 4_Winner.py
+++ 4_Winner.py
@@ -1,2 +1,14 @@
 def winner():
-    win1 = []
+    from beautifultable import BeautifulTable   #this creates 3 x 3 table = board
+    board = BeautifulTable()
+    board.append_row(['1', '2', '3'])
+    board.append_row(['4', '5', '6'])
+    board.append_row(['7', '8', '9'])
+    print(board)
+    board[1][0] = 'X'
+    board[1][1] = 'X'
+    board[1][2] = 'X'
+    if ['X', 'X', 'X'] in board:
+        print("Player 1 is the winner!")
+
+winner()
