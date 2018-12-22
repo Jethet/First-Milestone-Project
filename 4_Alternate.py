fce6c7a70acbbaab7491b0e88d5a84a1dcfd5426
jethet <henriette.hettinga@gmail.com>
2018-12-22 15:03:51 +0100

    Add else

--- 4_Alternate.py
+++ 4_Alternate.py
@@ -1,12 +1,21 @@
 
+from itertools import cycle
+
+def alternate():
+    for player in cycle([player 1, player 2]):
+
+
+
+
+"""
 def alternate():
     player = int(input("Choose betweeen 1 and 2. "))
     while True:
-        player == 1
-        print("Great")
+        if player == 1:
+            print("Great")
+        if player == 2:
+            print("Fine")
+        else:
+            print("This is not a valid choice.")
         break
-    while player == 2:
-        print("Fine")
-
-
-alternate()
+"""
