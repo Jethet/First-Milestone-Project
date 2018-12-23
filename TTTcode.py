3369cf1fd69bd962e26ce1adba5bd61dbe66af11
jethet <henriette.hettinga@gmail.com>
2018-12-23 09:40:52 +0100

    Test options for winning

--- TTTcode.py
+++ TTTcode.py
@@ -24,15 +24,48 @@ def choice():
         if player != 'X' and player != 'O':
             print("This is not a valid choice.")
             break
+        # Two options diagonal across with XXX:
+        if board[0][0] and board [1][1] and board[2][2] == 'X':
+            print("Player X is the winner!")
+            break
+        elif board[0][2] and board[1][1] and board[2][0] == 'X':
+            print("Player X is the winner!")
+            break
+        # Two options diagonal across with OOO:
+        if board[0][0] and board [1][1] and board[2][2] == 'O':
+            print("Player O is the winner!")
+            break
+        elif board[0][2] and board[1][1] and board[2][0] == 'O':
+            print("Player O is the winner!")
+            break
+        # Three options for vertical columns with XXX:
         if board[0][0] and board[1][0] and board[2][0] == 'X':
             print("Player X is the winner!")
             break
+        elif board[0][1] and board[1][1] and board[2][1] == 'X':
+            print("Player X is the winner!")
+            break
+        elif board[0][2] and board[1][2] and board[2][2] == 'X':
+            print("Player X is the winner!")
+            break
+        # Three options for vertical columns with OOO:
+        elif board[0][0] and board[1][0] and board[2][0] == 'O':
+            print("Player O is the winner!")
+            break
+        elif board[0][1] and board[1][1] and board[2][1] == 'O':
+            print("Player X is the winner!")
+            break
+        elif board[0][2] and board[1][2] and board[2][2] == 'O':
+            print("Player X is the winner!")
+            break
+        # Horizontal XXX and OOO options:
         elif ['X', 'X', 'X'] in board:
             print("Player X is the winner!")
             break
         elif ['O', 'O', 'O'] in board:
             print("Player X is the winner!")
             break
+
         player = input("Are you player X or player O? ")
         choice = input("Where do you place your mark? ")
         if choice == '1' and player == 'X':
