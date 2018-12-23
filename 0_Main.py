b53089dbecc805ea86d03f20a78512078046034a
jethet <henriette.hettinga@gmail.com>
2018-12-23 18:00:25 +0100

    Add comments to code

--- 0_Main.py
+++ 0_Main.py
@@ -1,27 +1,32 @@
 """
-TicTacToe: 2 players should be able to play the game (both sitting at
-the same computer).
-The board should be printed out every time a player makes a move.
-You should be able to accept input of the player position and then
-place a symbol on the board.
+This is the main() function of the code for the game TicTacToe.
+Two players can play the game, sitting at the same computer.
+The board should be printed out every time a player has made a move.
+The programme should accept input from the player, determine the
+position and then place a symbol on the board. Subsequently, that
+position should not be available anymore.
 """
-import pickle
 
+# This is an explanation of the game.
+print("The board has 3 x 3 = 9 squares. You can play with X or with O."
+" Each time you play you can mark one square.\n"
+"The player who gets three marks in a row (diagonal, horizontal or vertical)\n"
+"wins the game.\n")
+
+import pickle
+# Pickle is used to save all the  changes to the board during the game.
 with open('board', mode = 'wb') as my_file:  #this saves the board
     pickle.dump(board, my_file)
-    
-#First step is ask player to play and what choice (X or O)
-#Player is identified as player 1 or player 2; player 1 starts.
+
+# First step is ask players if they want to play.
+# Player is asked to choose X or O; player X starts.
 player()
 
-#If the player wants to play: show board and explain choices
+# If the player wants to play, a new board is created.
 new_board()
 
-#If player chooses a square, the choice is shown on the board
+# If player chooses a square, the choice is shown on the board.
 choice()
 
-#Declare winner
+# The player who gets three marks in a row is declared the winner.
 winner()
-
-#To clear screen between games: (?? do I need this?)
-print(\n*100)
