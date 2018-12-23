bc03e0a37644e028d0e69032879ff17fa8c72954
jethet <henriette.hettinga@gmail.com>
2018-12-23 09:46:16 +0100

    Include all options for choice of square

--- 2_Choice.py
+++ 2_Choice.py
@@ -1,4 +1,4 @@
-with open('board', mode = 'wb') as my_file:  #this is meant to save the board
+with open('board', mode = 'wb') as my_file:  #this saves the board
     pickle.dump(board, my_file)
 start = input("Would you like to play tic tac toe? Choose 1 for yes and 2 for no. ")
 if start == '2':
@@ -13,15 +13,6 @@ while True:
     square = ['1', '2', '3', '4', '5', '6', '7', '8', '9'] #3 x 3 squares on board
     if player != 'X' and player != 'O':
         print("This is not a valid choice.")
-        break
-    if board[0][0] and board[1][0] and board[2][0] == 'X':
-        print("Player X is the winner!")
-        break
-    elif ['X', 'X', 'X'] in board:
-        print("Player X is the winner!")
-        break
-    elif ['O', 'O', 'O'] in board:
-        print("Player X is the winner!")
         break
     player = input("Are you player X or player O? ")
     choice = input("Where do you place your mark? ")
@@ -53,6 +44,14 @@ while True:
         board[2][0] = 'X'
         square.remove('7')
         print(board)
+    elif choice == '8' and player == 'X':
+        board[2][1] = 'X'
+        square.remove('8')
+        print(board)
+    elif choice == '9' and player == 'X':
+        board[2][2] = 'X'
+        square.remove('9')
+        print(board)
     elif choice == '1' and player == 'O':
         board[0][0] = 'O'
         square.remove('1')
@@ -65,8 +64,33 @@ while True:
         board[0][2] = 'O'
         square.remove('3')
         print(board)
+    elif choice == '4' and player == 'O':
+        board[1][0] = 'O'
+        square.remove('4')
+        print(board)
+    elif choice == '5' and player == 'O':
+        board[1][1] = 'O'
+        square.remove('5')
+        print(board)
+    elif choice == '6' and player == 'O':
+        board[1][2] = 'O'
+        square.remove('6')
+        print(board)
+    elif choice == '7' and player == 'O':
+        board[2][0] = 'O'
+        square.remove('7')
+        print(board)
+    elif choice == '8' and player == 'O':
+        board[2][1] = 'O'
+        square.remove('8')
+        print(board)
+    elif choice == '9' and player == 'O':
+        board[2][2] = 'O'
+        square.remove('9')
+        print(board)
     else:
         print("This is not a valid choice.")
         return
+# Save board changes
 with open('board', mode = 'wb') as my_file:
     pickle.dump(board, my_file)
