932b619fb2de793cc7ddebdf86e3e59142c16b88
jethet <henriette.hettinga@gmail.com>
2019-01-10 17:04:45 +0100

    Use of True/False

--- Repetition.py
+++ Repetition.py
@@ -1,3 +1,15 @@
-def winner():
-    if square[[0][0], [1][0], [2][0]]== ['X', 'X', 'X']:
-        print("Win!")
+player = 'X' or 'O'
+square = ['1', '2', '3', '4', '5', '6', '7', '8', '9'] #3 x 3 squares on board
+square_taken = ['1']
+if player != 'X' and player != 'O':
+    print("This is not a valid choice.")
+
+game = True
+while game == True:
+    player = input("Are you player X or player O? ")
+    choice = input("Where do you place your mark? ")
+    if choice in square_taken:
+        print("This square is taken.")
+        continue
+    else:
+        game = False
