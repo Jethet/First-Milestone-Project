0ca83d3343dfde946c1170307a6d713c6693b738
jethet <henriette.hettinga@gmail.com>
2019-01-11 18:20:11 +0100

    Change numbering according to main()

---
+++ 2_NewBoard.py
@@ -0,0 +1,14 @@
+# This function creates the board for TicTacToe: nine squares, 3 x 3
+# A new board must be created each time a player answers 'yes' to the
+# question if he wants to play.
+# This function is part of main()
+
+def new_board():
+    from beautifultable import BeautifulTable
+    board = BeautifulTable()
+    board.append_row(['1', '2', '3'])
+    board.append_row(['4', '5', '6'])
+    board.append_row(['7', '8', '9'])
+    print(board, '\n')
+
+new_board()
