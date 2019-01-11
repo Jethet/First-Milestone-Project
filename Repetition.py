7400cb66a40a4c2f44ac389660eefcc1e55933cf
jethet <henriette.hettinga@gmail.com>
2019-01-11 12:04:42 +0100

    Test square_taken

--- Repetition.py
+++ Repetition.py
@@ -30,9 +30,11 @@ def choice():
 """
 import pickle
 from beautifultable import BeautifulTable   #this creates 3 x 3 table = board
+square = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 square_taken = []
 
-def board_coordinates(square):
+
+def board_coordinates(choice):
     board = BeautifulTable()
     board.append_row(['1', '2', '3'])
     board.append_row(['4', '5', '6'])
@@ -40,27 +42,37 @@ def board_coordinates(square):
     print(board)
     with open('board', mode = 'wb') as my_file:  #this is meant to save the board
         pickle.dump(board, my_file)
-    if square == '1':
-        square_taken.append('1')
-        return (0,0)
-    elif square == '2':
-        square_taken.append('2')
-        return (0,1)
-    elif square == '3':
-        square_taken.append('3')
-        return (0,2)
-    elif square == '4':
-        return (1,0)
-    elif square == '5':
-        return (1,1)
-    elif square == '6':
-        return (1,2)
-    elif square == '7':
-        return (2,0)
-    elif square == '8':
-        return (2,1)
-    elif square == '9':
-        return (2,2)
+        while True:
+            player = input("Are you player X or player O? ")
+            if player != 'X' and player != 'O':
+                print("This is not a valid choice.")
+                break
+            choice = input("Where do you place your mark? ")
+            if choice == '1':
+                square_taken.append('1')
+                #print(square_taken)
+                return (0,0)
+            elif choice == '2':
+                square_taken.append('2')
+                #print(square_taken)
+                return (0,1)
+            elif choice == '3':
+                square_taken.append('3')
+                return (0,2)
+            elif choice == '4':
+                return (1,0)
+            elif choice == '5':
+                return (1,1)
+            elif choice == '6':
+                return (1,2)
+            elif choice == '7':
+                return (2,0)
+            elif choice == '8':
+                return (2,1)
+            elif choice == '9':
+                return (2,2)
 
 first_element, second_element = board_coordinates('1')
 print(first_element, second_element)
+
+board_coordinates(square)
