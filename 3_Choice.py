41f53a7218070f78c6a2d912c8d585fabf95c8ff
jethet <henriette.hettinga@gmail.com>
2019-01-12 19:04:31 +0100

    Code for player choice

--- 3_Choice.py
+++ 3_Choice.py
@@ -3,7 +3,7 @@
 # This function is part of main()
 
 import pickle
-from beautifultable import BeautifulTable   #this creates 3 x 3 table = board
+from beautifultable import BeautifulTable
 board = BeautifulTable()
 board.append_row(['1', '2', '3'])
 board.append_row(['4', '5', '6'])
@@ -12,21 +12,29 @@ print(board)
 
 # The function starts here:
 def choice():
-
-    with open('board', mode = 'wb') as my_file:  #this is meant to save the board
-        pickle.dump(board, my_file)
-    square = ['1', '2', '3', '4', '5', '6', '7', '8', '9'] #3 x 3 squares on board
+    square = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
     square_taken = []
     while True:
         choice = input("Where do you place your mark? ")
         if choice in square_taken:
             print("This square is taken.")
             continue
-        elif choice not in square_taken:
-            board_coordinates(choice)
+        if choice not in square_taken:
+# HERE the programme should a) determine the coordinates and b) the player
+# The board_coordinates can be used for X and for O but how??
+# And how to use the function board_coordinates inside choice()??
+# This needs to be brought together: get the square and place X or O.
+
+            if player == 'X':
+                board_coordinates(choice)
+                print('X')
+            elif player == 'O':
+                board_coordinates(choice)
+                print('O')
         else:
             print("This is not a valid choice.")
             return
+
     # Save board changes
     with open('board', mode = 'wb') as my_file:
         pickle.dump(board, my_file)
