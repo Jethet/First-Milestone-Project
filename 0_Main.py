edbd1f9e4014fb098b2bc2f76e58870dcbee3de3
jethet <henriette.hettinga@gmail.com>
2019-01-13 13:57:45 +0100

    Add winner() to main()

--- 0_Main.py
+++ 0_Main.py
@@ -79,4 +79,17 @@ def board_coordinates(choice):
             pickle.dump(board, my_file)
 
 # The player who gets three marks in a row is declared the winner.
-winner()
+def winner():
+    player = 'X' or 'O'
+    if board[0][0] and board[1][1] == board[2][2] or board[0][2] and \
+       board[1][1] == board[2][0] or board[0][0] and board[0][1] == \
+       board[0][2] or board[1][0] and board[1][1] == board[1][2] or \
+       board[2][0] and board[2][1] == board[2][2] or board[0][0] and \
+       board[1][0] == board[2][0] or board[0][1] and board[1][1] \
+       == board[2][1] or board[0][2] and board[1][2] == board[2][2]:
+        if player == 'X':
+            print("Player X is the winner!")
+            break
+        elif player == 'O':
+            print("Player O is the winner!")
+            break
