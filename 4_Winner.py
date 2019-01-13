a6e28ed834e03a14d0c7c14899915ba2f565c0bb
jethet <henriette.hettinga@gmail.com>
2019-01-13 11:37:48 +0100

    Add code to assign winner

--- 4_Winner.py
+++ 4_Winner.py
@@ -1,49 +1,34 @@
 # This function returns a 'winner!' message if a player has won the game.
-# There are many options for getting three X or three O in a row: horizontal,
+# There are many options for getting three in a row: horizontal,
 # vertical or diagonal.
 # This function is part of main()
 
 def winner():
-    # Two options diagonal across with XXX:
-    if board[0][0] and board [1][1] and board[2][2] == 'X':
+    # For horizontal three in a row:
+    if ['X', 'X', 'X'] in board:
         print("Player X is the winner!")
         break
-    elif board[0][2] and board[1][1] and board[2][0] == 'X':
-        print("Player X is the winner!")
-        break
-    # Two options diagonal across with OOO:
-    if board[0][0] and board [1][1] and board[2][2] == 'O':
+    if ['O', 'O', 'O'] in board:
         print("Player O is the winner!")
         break
-    elif board[0][2] and board[1][1] and board[2][0] == 'O':
-        print("Player O is the winner!")
-        break
-    # Three options for vertical columns with XXX:
-    if board[0][0] and board[1][0] and board[2][0] == 'X':
-        print("Player X is the winner!")
-        break
-    elif board[0][1] and board[1][1] and board[2][1] == 'X':
-        print("Player X is the winner!")
-        break
-    elif board[0][2] and board[1][2] and board[2][2] == 'X':
-        print("Player X is the winner!")
-        break
-    # Three options for vertical columns with OOO:
-    elif board[0][0] and board[1][0] and board[2][0] == 'O':
-        print("Player O is the winner!")
-        break
-    elif board[0][1] and board[1][1] and board[2][1] == 'O':
-        print("Player X is the winner!")
-        break
-    elif board[0][2] and board[1][2] and board[2][2] == 'O':
-        print("Player X is the winner!")
-        break
-    # Horizontal XXX and OOO options:
-    elif ['X', 'X', 'X'] in board:
-        print("Player X is the winner!")
-        break
-    elif ['O', 'O', 'O'] in board:
-        print("Player X is the winner!")
-        break
+
+    for player == 'X' or player == 'O':
+        # Two options diagonal three in a row:
+        if choice[0][0] and choice[1][1] == choice[2][2]:
+            print(player, " is the winner!")
+            break
+        elif choice[0][2] and choice[1][1] == choice[2][0]:
+            print(player, " is the winner!")
+            break
+        # Three options vertical three in a row:
+        if choice[0][0] and choice[1][0] == choice[2][0]:
+            print(player, " is the winner!")
+            break
+        elif choice[0][1] and choice[1][1] == choice[2][1]:
+            print(player, " is the winner!")
+            break
+        elif choice[0][2] and choice[1][2] == choice[2][2]:
+            print(player, " is the winner!")
+            break
 
 winner()
