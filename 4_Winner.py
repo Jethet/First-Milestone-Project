597a83e2eefde387f51429d59be4e8ecaf89ad9b
jethet <henriette.hettinga@gmail.com>
2019-01-22 12:11:22 +0100

    Error re. break outside loop

--- 4_Winner.py
+++ 4_Winner.py
@@ -5,22 +5,22 @@
 def winner():
     player = 'X' or 'O'
     if board[0][0] and board[1][1] == board[2][2] or board[0][2] and \
-       board[1][1] == board[2][0] or board[0][0] and board[0][1] == \
-       board[0][2] or board[1][0] and board[1][1] == board[1][2] or \
-       board[2][0] and board[2][1] == board[2][2] or board[0][0] and \
-       board[1][0] == board[2][0] or board[0][1] and board[1][1] \
-       == board[2][1] or board[0][2] and board[1][2] == board[2][2] and \
-       player == 'X':
-       print("Player X is the winner!")
+          board[1][1] == board[2][0] or board[0][0] and board[0][1] == \
+          board[0][2] or board[1][0] and board[1][1] == board[1][2] or \
+          board[2][0] and board[2][1] == board[2][2] or board[0][0] and \
+          board[1][0] == board[2][0] or board[0][1] and board[1][1] \
+          == board[2][1] or board[0][2] and board[1][2] == board[2][2] \
+          and player == 'X':
+        print("Player X is the winner!")
     break
     if board[0][0] and board[1][1] == board[2][2] or board[0][2] and \
-       board[1][1] == board[2][0] or board[0][0] and board[0][1] == \
-       board[0][2] or board[1][0] and board[1][1] == board[1][2] or \
-       board[2][0] and board[2][1] == board[2][2] or board[0][0] and \
-       board[1][0] == board[2][0] or board[0][1] and board[1][1] \
-       == board[2][1] or board[0][2] and board[1][2] == board[2][2] and \
-       player == 'O':
-       print("Player O is the winner!")
+          board[1][1] == board[2][0] or board[0][0] and board[0][1] == \
+          board[0][2] or board[1][0] and board[1][1] == board[1][2] or \
+          board[2][0] and board[2][1] == board[2][2] or board[0][0] and \
+          board[1][0] == board[2][0] or board[0][1] and board[1][1] \
+          == board[2][1] or board[0][2] and board[1][2] == board[2][2] \
+          and player == 'O':
+        print("Player O is the winner!")
     break
 
 winner()
