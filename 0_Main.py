94ed938488809d495965ca8ebd47b04635f258bb
jethet <henriette.hettinga@gmail.com>
2019-01-25 11:55:58 +0100

    Move up code for player input

--- 0_Main.py
+++ 0_Main.py
@@ -42,15 +42,18 @@ def new_board():
 # If player chooses a square, the choice is shown on the board.
 # The chosen square should be blocked.
 def choice(player, board):
-    # make a list of squares that can be chosen
-    list_available_squares = open_square(board)
     # ask the player for her/his choice
     player_choice = input("Where do you place your mark? ")
+
+    # make a list of squares that can be chosen
+    list_available_squares = open_square(board)
+
     # check if choice is in the list of squares
     # if choice not in list of squares, player must choose again
     while player_choice not in list_available_squares:
         print("This square is taken. ")
         player_choice = input("Where do you place your mark? ")
+        
     # choice is translated into board_coordinates
     board_coordinates = transform_choice(choice)
     # show choice on the board
