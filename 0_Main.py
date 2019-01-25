ff0354caf3ca3195f0c236f6b2830ac0a0714d68
jethet <henriette.hettinga@gmail.com>
2019-01-25 12:54:57 +0100

    Integrate new function transform_choice(choice) into main()

--- 0_Main.py
+++ 0_Main.py
@@ -74,10 +74,10 @@ def open_square():
         return available_square
 
 def transform_choice(choice):
-    board_coordinates = {1:(0,0), 2:(0,1), 3:(0,2), 4:(1.0), 5:(1,1), 6:(1,2),\
+    board_squares = {1:(0,0), 2:(0,1), 3:(0,2), 4:(1.0), 5:(1,1), 6:(1,2),\
                           7:(2,0), 8:(2,1), 9:(2,2)}
-    square = board_coordinates.get(choice)
-    return square
+    board_coordinates = board_squares.get(choice)
+    return board_coordinates
 
 def update_board(player, board_coordinates, board):
     # adapt board by adding player choice on relevant square
