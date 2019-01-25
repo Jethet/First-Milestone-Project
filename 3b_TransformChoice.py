609b32fb231f9d664c4b7d72fc7adebcc5b3e19d
jethet <henriette.hettinga@gmail.com>
2019-01-25 12:54:17 +0100

    Created and tested new function transform_choice

--- 3b_TransformChoice.py
+++ 3b_TransformChoice.py
@@ -2,9 +2,9 @@
 #  board_coordinates = transform_choice(choice)
 
 def transform_choice(choice):
-    board_coordinates = {1:(0,0), 2:(0,1), 3:(0,2), 4:(1.0), 5:(1,1), 6:(1,2),\
+    board_squares = {1:(0,0), 2:(0,1), 3:(0,2), 4:(1.0), 5:(1,1), 6:(1,2),\
                           7:(2,0), 8:(2,1), 9:(2,2)}
-    square = board_coordinates.get(choice)
-    return square
+    board_coordinates = board_squares.get(choice)
+    return board_coordinates
 
 print(transform_choice(9))
