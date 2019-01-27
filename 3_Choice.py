28a00e92f9b575d3641e3e59b4b5f6f66fd1266a
jethet <henriette.hettinga@gmail.com>
2019-01-27 18:08:19 +0100

    Add new functions open_square(), transform_choice() and update_board()

--- 3_Choice.py
+++ 3_Choice.py
@@ -22,15 +22,55 @@ def choice(player, board):
 
 
 def open_square(board):
+    # Calculate available squares
+    player_choice = input("Where do you place your mark? ")
     available_square = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-    # TO DO: calculate available squares
+    while player_choice not in available_square:
+        print("This is not a valid choice. ")
+        player_choice = input("Where do you place your mark? ")
+    if player_choice in available_square:
+        available_square.remove(player_choice)
+        return available_square
     return available_square
 
 def transform_choice(choice):
-    # TO DO: write board_coordinates
-    return (0,0)
+    board_squares = {1:(0,0), 2:(0,1), 3:(0,2), 4:(1.0), 5:(1,1), 6:(1,2),\
+                          7:(2,0), 8:(2,1), 9:(2,2)}
+    board_coordinates = board_squares.get(choice)
+    return board_coordinates
 
 def update_board(player, board_coordinates, board):
     # adapt board by adding player choice on relevant square
-    # TO DO: code for updating board
+    if board_coordinates == (0,0):
+        board[0][0] = player
+        print(board)
+    elif board_coordinates == (0,1):
+        board[0][1] = player
+        print(board)
+    elif board_coordinates == (0,2):
+        board[0][2] = player
+        print(board)
+    elif board_coordinates == (1,0):
+        board[1][0] = player
+        print(board)
+    elif board_coordinates == (1,1):
+        board[1][1] = player
+        print(board)
+    elif board_coordinates == (1,2):
+        board[1][2] = player
+        print(board)
+    elif board_coordinates == (2,0):
+        board[2][0] = player
+        print(board)
+    elif board_coordinates == (2,1):
+        board[2][1] = player
+        print(board)
+    elif board_coordinates == (2,2):
+        board[2][2] = player
+        print(board)
+    else:
+        print("This is not a valid choice.")
+
     return updated_board
+
+choice()
