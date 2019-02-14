afa97cc3dba4edec00961659aeae665cd50a5b31
jethet <henriette.hettinga@gmail.com>
2019-02-14 16:39:57 +0100

    Function transform_choice() runs OK

--- 4b_TransformChoice.py
+++ 4b_TransformChoice.py
@@ -1,10 +1,10 @@
 # choice is translated into board_coordinates:
 #  board_coordinates = transform_choice(choice)
 
-def transform_choice(choice):
+def transform_choice(player_choice):
     board_squares = {1:(0,0), 2:(0,1), 3:(0,2), 4:(1.0), 5:(1,1), 6:(1,2),\
                           7:(2,0), 8:(2,1), 9:(2,2)}
-    board_coordinates = board_squares.get(choice)
+    board_coordinates = board_squares.get(player_choice)
     return board_coordinates
 
 print(transform_choice(9))
