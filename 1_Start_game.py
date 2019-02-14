51eee3fc17ada15a3ae5ffb47f2d040c097ff9a0
jethet <henriette.hettinga@gmail.com>
2019-02-14 11:16:47 +0100

    Use while loop to repeat option to choose

--- 1_Start_game.py
+++ 1_Start_game.py
@@ -1,10 +1,14 @@
 def start_game():
-    start = input("Would you like to play tic tac toe? Choose 1 for yes and 2 for no. ")
-    if start == '2':
-        print("Game over.")
-        return
-    elif start != '1' and start !='2':
-        print("This is not valid. Please enter 1 or 2.")
-        return
-    else:
-        print("Player X starts the game.")
+    while True:
+        start = input("Would you like to play tic tac toe? Choose 1 for yes\
+ and 2 for no. ")
+        if start == '2':
+            print("Game over.")
+            break
+        elif start != '1' and start !='2':
+            print("This is not valid. Please enter 1 or 2.")
+        else:
+            print("Player X starts the game.")
+            break
+
+start_game()
