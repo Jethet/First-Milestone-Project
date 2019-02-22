3608a55731aed10bf69adf79a4a9ff6200f05085
jethet <henriette.hettinga@gmail.com>
2019-02-22 15:19:12 +0100

    Comment on possible bug

--- 5_Winner.py
+++ 5_Winner.py
@@ -2,26 +2,17 @@
 # Two diagonal, three horizontal and three vertical options to win.
 # This function is part of main()
 
-def winner():
-    player = 'X' or 'O'
+def check_winner(board):
     if board[0][0] and board[1][1] == board[2][2] or board[0][2] and \
-          board[1][1] == board[2][0] or board[0][0] and board[0][1] == \
-          board[0][2] or board[1][0] and board[1][1] == board[1][2] or \
-          board[2][0] and board[2][1] == board[2][2] or board[0][0] and \
-          board[1][0] == board[2][0] or board[0][1] and board[1][1] \
-          == board[2][1] or board[0][2] and board[1][2] == board[2][2] \
-          and player == 'X':
-        print("Player X is the winner!")
-        break
-    elif board[0][0] and board[1][1] == board[2][2] or board[0][2] and \
-          board[1][1] == board[2][0] or board[0][0] and board[0][1] == \
-          board[0][2] or board[1][0] and board[1][1] == board[1][2] or \
-          board[2][0] and board[2][1] == board[2][2] or board[0][0] and \
-          board[1][0] == board[2][0] or board[0][1] and board[1][1] \
-          == board[2][1] or board[0][2] and board[1][2] == board[2][2] \
-          and player == 'O':
-        print("Player O is the winner!")
+       board[1][1] == board[2][0] or board[0][0] and board[0][1] == \
+       board[0][2] or board[1][0] and board[1][1] == board[1][2] or \
+       board[2][0] and board[2][1] == board[2][2] or board[0][0] and \
+       board[1][0] == board[2][0] or board[0][1] and board[1][1] \
+       == board[2][1] or board[0][2] and board[1][2] == board[2][2]:
+       return True
     else:
-        print("It is a tie!")
-
-winner()
+        print("It is a tie!")  # IF THE CHECK FOR A WINNER IS DONE EACH TIME
+        return False            # THE PLAYER CHOOSES A SQUARE, THERE WILL BE
+                                # MANY TIMES THAT 3-IN-A-ROW IS NOT True
+                                # The message 'It is a tie!' should not be
+                                # printed EVERY time
