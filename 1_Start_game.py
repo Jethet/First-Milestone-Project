a8064767d78a7b533777b8c9f9a2fe15dc21333f
jethet <henriette.hettinga@gmail.com>
2019-02-22 16:21:28 +0100

    Try/except and exit()

--- 1_Start_game.py
+++ 1_Start_game.py
@@ -1,13 +1,18 @@
 def start_game():
     start = input("Would you like to play tic tac toe? Choose 1 for yes\
  and 2 for no. ")
-    while start != '1' and start != '2':
-        print("This is not valid. Please enter 1 or 2.")
-    else:
-        if start == '2':
+    try:
+        while start != '1' and start != '2':
+            print("This is not valid. Please enter 1 or 2.")
+            start = input("Choose 1 for yes and 2 for no. ")
+        else:
+            if start == '1':
+                print("Let's play!")
+
+    except:
             print("Game over!")
             exit()
-        elif start == '1':
-            print("Let's play!")
+    print("Test")
+
 
 start_game()
