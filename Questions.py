d52fbd2bcfbe0fa7899eb14b72a0d66dad31aa31
jethet <henriette.hettinga@gmail.com>
2019-02-22 11:50:17 +0100

    Added question re. error

--- Questions.py
+++ Questions.py
@@ -8,14 +8,16 @@ with open('board', mode = 'wb') as handle:  #this saves the board
 """
 after the function new_board() in main() because in my thinking the board file
 is created in the function new_board() and should be saved as a file to use in
-the rest of the programme.
+the rest of the programme. ERROR: name 'board' is not defined.
+I do not understand this because the board is defined in the function new_board()
+and returned. For now, I have grayed it out.
 
-To access the file I have now put
+To access the file I have  put
 """
 with open('board', mode = 'rb') as handle:
     board = pickle.load(handle)
 """
-after the function  choice().
+after the function  choice(). For now, I have grayed this out as well.
 
 
 Other question in general re. pickle: I know that the 'b' in 'wb' and 'rb'
