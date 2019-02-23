5077680f01182adf6461e15b34ced2ef71694bb8
jethet <henriette.hettinga@gmail.com>
2019-02-23 16:40:34 +0100

    Test improved while loop

--- 4a_OpenSquare.py
+++ 4a_OpenSquare.py
@@ -1,18 +1,38 @@
 # make a list of squares that can be chosen. Update list with choice of player
 # so that squares that are taken are no longer on the available_square list.
-
+"""
 def open_square():
-    player_choice = input("Where do you place your mark? ")
     available_square = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-    while player_choice not in available_square:
+    player_choice = input("Where do you place your mark? ")
+    try:
+        if player_choice not in available_square:
+            print("This is not a valid choice. ")
+        else:
+            available_square.remove(player_choice)
+            return available_square
+            return player_choice
+        #player_choice = input("Where do you place your mark? ")
+    except Exception as e:
         print("This is not a valid choice. ")
-        player_choice = input("Where do you place your mark? ")
-
-    available_square.remove(player_choice)
-    return available_square
-    #print(available_square)
-    return player_choice
-    #print(player_choice)
-
 
 open_square()
+"""
+
+#TEST
+def open_square():
+    available_square = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    player_choice = input("Where do you place your mark? ")
+    try:
+        while player_choice not in available_square:
+            print("This is not a valid choice. ")
+            player_choice = input("Where do you place your mark? ")
+        else:
+            available_square.remove(player_choice)
+            print(available_square)
+            print(player_choice)
+        #player_choice = input("Where do you place your mark? ")
+    except ValueError:
+        pass
+
+open_square()
+#
