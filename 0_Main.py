78ee83c74dcd39a45aafbb543fbdb3d4f1828127
jethet <henriette.hettinga@gmail.com>
2019-03-07 11:37:12 +0100

    Test new version of check_winner(board); bug discovered

--- 0_Main.py
+++ 0_Main.py
@@ -130,6 +130,26 @@ def get_choice(player, board):
 # The player who gets three marks in a row is declared the winner.
 def check_winner(board):
     if board[0][0] and board[1][1] and board[2][2] == 'X' or board[0][2] and \
+       board[1][1] and board[2][0] == 'X' or board[0][0] and board[0][1] and \
+       board[0][2] == 'X' or board[1][0] and board[1][1] and board[1][2] == 'X' \
+       or board[2][0] and board[2][1] and board[2][2] == 'X' or board[0][0] and \
+       board[1][0] and board[2][0] == 'X' or board[0][1] and board[1][1] \
+       and board[2][1] == 'X' or board[0][2] and board[1][2] and board[2][2] \
+       == 'X' or board[0][0] and board[1][1] and board[2][2] == 'O' or board[0][2] \
+       and board[1][1] and board[2][0] == 'O' or board[0][0] and board[0][1] and \
+       board[0][2] == 'O' or board[1][0] and board[1][1] and board[1][2] == 'O' \
+       or board[2][0] and board[2][1] and board[2][2] == 'O' or board[0][0] and \
+       board[1][0] and board[2][0] == 'O' or board[0][1] and board[1][1] \
+       and board[2][1] == 'O' or board[0][2] and board[1][2] and board[2][2] \
+       == 'O' :
+        print("We have a winner!")
+        return True
+    else:
+        print("No winner!")
+        return False
+"""
+def check_winner(board):
+    if board[0][0] and board[1][1] and board[2][2] == 'X' or board[0][2] and \
        board[1][1] == board[2][0] or board[0][0] and board[0][1] == \
        board[0][2] or board[1][0] and board[1][1] == board[1][2] or \
        board[2][0] and board[2][1] == board[2][2] or board[0][0] and \
@@ -140,7 +160,7 @@ def check_winner(board):
     else:
         print("No winner!")
         return False
-
+"""
 # When the game ends, the player is asked if s/he wants to play again:
 """
 def repeat_game():
--- 5_Winner.py
+++ 5_Winner.py
@@ -4,11 +4,12 @@
 
 def check_winner(board):
     if board[0][0] and board[1][1] and board[2][2] == 'X' or board[0][2] and \
-       board[1][1] == board[2][0] or board[0][0] and board[0][1] == \
-       board[0][2] or board[1][0] and board[1][1] == board[1][2] or \
-       board[2][0] and board[2][1] == board[2][2] or board[0][0] and \
-       board[1][0] == board[2][0] or board[0][1] and board[1][1] \
-       == board[2][1] or board[0][2] and board[1][2] == board[2][2]:
+       board[1][1] and board[2][0] == 'X' or board[0][0] and board[0][1] and \
+       board[0][2] == 'X' or board[1][0] and board[1][1] and board[1][2] == 'X' \
+       or board[2][0] and board[2][1] and board[2][2] == 'X' or board[0][0] and \
+       board[1][0] and board[2][0] == 'X' or board[0][1] and board[1][1] \
+       and board[2][1] == 'X' or board[0][2] and board[1][2] and board[2][2] \
+       == 'X' or board[0][0] and board[1][1] and board[2][2] == 'O':
         print("We have a winner!")
         return True
     else:
