170c2a95a670b9521486a0941a2ea1ce9fdc941f
jethet <henriette.hettinga@gmail.com>
2019-04-12 16:13:42 +0200

    Delete questions that have been answered

--- Questions.py
+++ Questions.py
@@ -1,5 +1,5 @@
 """
-Questions for David:
+Question for David:
 
 I added:
 """
@@ -24,23 +24,5 @@ after running the programme but that seems too much? It is now grayed out.
 Other question in general re. pickle: I know that the 'b' in 'wb' and 'rb'
 stands for 'binary'. I also see code that just uses 'r' and 'w'.
 Is there a best practice for this?
-
-z
-Next question: how to repeat the game? I wrote a function but there are problems with it:
-
--  when repeating the game, the new list of numbers is not used and the
-   programme continues to use the previous list, i.e. with the numbers that
-   are left from the previous game;
-
--  in some cases, after only one or two choices, the programme already declares
-   a winner; it seems the choices of the previous game are still used even
-   though the list of available squares is renewed and the board is new;
-
--  the programme also declares a winner when 3 in a row is X X O;
-
--  HOW TO REPEAT GAME WITHOUT FUNCTIONS start_game() AND new_board()?
-
--  should pickle/unpickle be used? when and how?
-
 
 """
